85635be0ddb907ea05808886ec2b240fdec01b2d
AtosGuru <atosguru7@gmail.com>
2022-04-07 13:58:00 -0700

    changed font size

--- components/HowHelpUs.js
+++ components/HowHelpUs.js
@@ -7,9 +7,9 @@ export const HowHelpUs = () => {
         <div className='container px-5 py-24 mx-auto px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
          
           <div className='flex flex-wrap -m-4 '>
-            <div className='container px-5 py-24 mx-auto px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
+            <div className='container px-5 py-24 mx-auto px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20' style={{height: '400px'}}>
                 <Fade up>
-                  <h2 className=' max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto' style={{textAlign: 'center'}}>
+                  <h2 className='a-how-title max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto' >
                     <span className='relative inline-block'>
                       <svg
                         viewBox='0 0 52 24'
@@ -23,36 +23,36 @@ export const HowHelpUs = () => {
                         </defs>
                         {/* <rect fill='url(#700c93bf-0068-4e32-aafe-ef5b6a647708)' width='52' height='24' /> */}
                       </svg>
-                      <span className='relative'>Want</span>
+                     
                     </span>{' '}
                     How Can Automation and Robotics help you?
                   </h2>
                 </Fade>
                 <LightSpeed left>
-                  <p className='text-base text-indigo-100 md:text-lg '>
+                  <p className='text-base text-indigo-100 md:text-lg a-p'>
                   Robots and Automation offer a more reliable alternative to human employees because they provide consistency in performance, quality, and efficiency, allowing us to promotelabortohigher-value tasks.  They can also perform tasks that are too dangerous for humans to do, such as working with hazardous materials, or tasks that are physically exhausting for humans.
                   </p>
                 </LightSpeed>
                 </div>
                 <Jello>
-                    <div class="grid grid-flow-col grid-rows-1 grid-cols-5 gap-5 img-div" style={{margin: 'auto'}}>
-                      <div className="">
+                    <div class="row grid grid-flow-col grid-rows-1 grid-cols-5 gap-5 img-div" style={{margin: 'auto'}}>
+                      <div className="col-md-2">
                         <img src="/img/Reliable.svg" alt="" />
                         <h3 className="how-help">Reliable</h3>
                       </div>
-                      <div >
+                      <div className="col-md-2">
                         <img src="/img/Increase efficiency.svg" alt="" />
                         <h3 className="how-help">Increase efficiency</h3>
                       </div>
-                      <div >
+                      <div className="col-md-2">
                         <img src="/img/Precise.svg" alt="" />
                         <h3 className="how-help">Precise</h3>
                       </div>
-                      <div >
+                      <div className="col-md-2">
                         <img src="/img/Save Time.svg" alt="" />
                         <h3 className="how-help">Save Time</h3>
                       </div>
-                      <div >
+                      <div className="col-md-2">
                         <img src="/img/Save Money.svg" alt="" />
                         <h3 className="how-help">Save Money</h3>
                       </div>
--- components/test.js
+++ components/test.js
@@ -11,20 +11,22 @@ export const Test = () => {
         <div class='overlay'></div>
         <div class='text text-center'>
           <div className=' mb-6'>
-            <h2 className=' mb-6 font-sans font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
+            {/* <h2 className=' mb-6 font-sans font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none'>
               <span style={{fontSize: "50px"}}>
                 <span className=' text-4xl xl:inline'>Let Robots Do your </span>
                 <span className=' text-4xl text-red-500  xl:inline'>Jobs </span>
               </span>
-            </h2>
-            <p className='text-base text-gray-700 md:text-lg'>
+            </h2> */}
+            <h1 className="a-title font-sans font-bold text-gray-900 tracking-tight">Let Robots Do Your <span className="a-job text-red-500 ">Jobs</span></h1>
+            
+            <p className='text-base text-gray-700 md:text-lg text-center a-p '>
               We offer intelligent automation solutions that are designed to take over repetitive, menial tasks and
               increase efficiency.
             </p>
             <a
               href='#'
               className='inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-red-500 hover:text-gray-900 focus:shadow-outline focus:outline-none'
-              style={{ background: '#ff3333', color: 'white' }}
+              style={{ background: '#ff3333', color: 'white', marginTop: "40px" }}
             >
               Get a Quote
             </a>
